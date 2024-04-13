f48f91c71ac15152263b8fc6213ac8fe05b3f5b2
MatPaluch <xp.mateusz@gmail.com>
2024-04-13 20:13:56 +0200

    Task_1&Task_2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('BODY');
+let timerID = null;
+stopBtn.setAttribute('disabled', '');
+
+startBtn.addEventListener('click', e => {
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled');
+  timerID = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 500);
+});
+
+stopBtn.addEventListener('click', e => {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', '');
+  clearInterval(timerID);
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,104 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+require('flatpickr/dist/themes/material_blue.css');
+
+// all modules
+
+import Notiflix from 'notiflix';
+
+// one by one
+
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Report } from 'notiflix/build/notiflix-report-aio';
+// import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+// import { Loading } from 'notiflix/build/notiflix-loading-aio';
+// import { Block } from 'notiflix/build/notiflix-block-aio';
+
+const flatpickr = require('flatpickr');
+const startBtn = document.querySelector('[data-start]');
+startBtn.setAttribute('disabled', '');
+
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+const input = document.querySelector('#datetime-picker');
+input.classList.add('border');
+
+let leftTime = null;
+let idInterval = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  locale: {
+    firstDayOfWeek: 1, // start week on Monday
+  },
+  onClose(selectedDates) {
+    const today = new Date().getTime();
+    const selectedTime = selectedDates[0].getTime();
+    if (selectedTime - today > 0) {
+      startBtn.removeAttribute('disabled');
+      leftTime = selectedTime - today;
+      Notiflix.Report.success(
+        'CORRECT DATE',
+        '"Now time will pass ;)"',
+        'Okay'
+      );
+    } else {
+      startBtn.setAttribute('disabled', '');
+      Notiflix.Report.failure(
+        'INCORRECT DATE',
+        'Please choose a date in the future',
+        'Okay'
+      );
+    }
+  },
+  onOpen() {
+    input.classList.add('change');
+  },
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+flatpickr('#datetime-picker', options);
+
+startBtn.addEventListener('click', e => {
+  startBtn.setAttribute('disabled', '');
+  idInterval = setInterval(() => {
+    const convertedTime = convertMs(leftTime);
+
+    days.textContent = addLeadingZero(convertedTime.days);
+    hours.textContent = addLeadingZero(convertedTime.hours);
+    minutes.textContent = addLeadingZero(convertedTime.minutes);
+    seconds.textContent = addLeadingZero(convertedTime.seconds);
+
+    leftTime = leftTime - 1000;
+    if (leftTime <= 0) {
+      clearInterval(idInterval);
+    }
+  }, 1000);
+});
