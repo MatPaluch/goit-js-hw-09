daadf3c56b1cbe7afc921bbad37cbff980e5848d
MatPaluch <150561295+MatPaluch@users.noreply.github.com>
2024-04-14 15:12:04 +0000

    Deploying to gh-pages from @ MatPaluch/goit-js-hw-09@eb388354d25586cb7ce703b480080cd33c85a688 🚀

--- 03-promises.7e72441c.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",e=>{e.preventDefault();let t=e.target.elements,o=t.delay.value,l=t.step.value,n=t.amount.value;for(let e=1;e<=n;e++)(function(e,t){return new Promise((o,l)=>{setTimeout(()=>{Math.random()>.3?o({position:e,delay:t}):l({position:e,delay:t})},t)})})(e,o).then(({position:e,delaySuc:t})=>{console.log(`\u{2705} Fulfilled promise ${e} in ${t}ms`)}).catch(({position:e,delayEr:t})=>{console.log(`\u{274C} Rejected promise ${e} in ${t}ms`)}),o+=l});
-//# sourceMappingURL=03-promises.7e72441c.js.map
